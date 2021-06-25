17e872d2bcf6d3b9476923bcdd24b030c68ef4f4
andreammgcol <amendez72@misena.edu.co>
2021-06-25 15:58:55 -0500

    Adding CSS & Images mandatory

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/dashboard_main.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: ['file-loader',
+                    {loader: 'image-webpack-loader'},
+                ],
+            },
+        ],
+    },
+};
