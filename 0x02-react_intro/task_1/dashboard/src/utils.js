9777ebce998e28ab596f87475f530ebff788a62d
andreammgcol <amendez72@misena.edu.co>
2021-06-29 22:07:09 -0500

    delete console.log

--- 0x02-react_intro/task_1/dashboard/src/utils.js
+++ 0x02-react_intro/task_1/dashboard/src/utils.js
@@ -1,7 +1,6 @@
 export const getFullYear = () => new Date().getFullYear();
 
 export const getFooterCopy = (isIndex) => {
-    console.log(isIndex);
     if (isIndex) {
         return "Holberton School";
     } else {
